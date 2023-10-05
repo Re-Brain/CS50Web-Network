c9e0529323bbcd61356fa744132d1657b43b401d
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2023-10-05 15:31:44 +0800

    Finished the database layout

--- network/models.py
+++ network/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+    follower = models.ManyToManyField("User", related_name="your_following", blank=True)
+    following = models.ManyToManyField("User", related_name="your_follower", blank=True)
+    post = models.ManyToManyField("Post", related_name="poster", blank=True)
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+    like = models.ManyToManyField("User", related_name="liker", null=True, blank=True)
